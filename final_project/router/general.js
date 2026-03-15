dfb4a008cf1cda2f9d5654fedf25484c5366d929
adhnannp <adhnanusman1234@gmail.com>
2026-03-15 17:48:58 +0530

    Assignment complete: added promises for API endpoints and auth user login/reviews

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,38 +6,90 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username && password) {
+    if (!isValid(username)) {
+      users.push({"username":username,"password":password});
+      return res.status(200).json({message: "User successfully registred. Now you can login"});
+    } else {
+      return res.status(404).json({message: "User already exists!"});
+    }
+  }
+  return res.status(404).json({message: "Unable to register user."});
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let myPromise = new Promise((resolve,reject) => {
+    resolve(books);
+  });
+  myPromise.then((successMessage) => {
+    return res.status(200).json(successMessage);
+  })
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+  let myPromise = new Promise((resolve,reject) => {
+    const isbn = req.params.isbn;
+    resolve(books[isbn]);
+  });
+  myPromise.then((successMessage) => {
+    return res.status(200).json(successMessage);
+  })
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let myPromise = new Promise((resolve,reject) => {
+    const author = req.params.author;
+    const matchingBooks = Object.values(books).filter(book => book.author === author);
+    resolve(matchingBooks);
+  });
+  myPromise.then((successMessage) => {
+    return res.status(200).json(successMessage);
+  })
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let myPromise = new Promise((resolve,reject) => {
+    const title = req.params.title;
+    const matchingBooks = Object.values(books).filter(book => book.title === title);
+    resolve(matchingBooks);
+  });
+  myPromise.then((successMessage) => {
+    return res.status(200).json(successMessage);
+  })
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  return res.status(200).json(books[isbn].reviews);
+});
+
+public_users.get('/books', function (req, res) {
+  return res.status(200).json(books);
+});
+
+public_users.get('/books/isbn/:isbn', function (req, res) {
+  const isbn = req.params.isbn;
+  return res.status(200).json(books[isbn]);
+});
+
+public_users.get('/books/author/:author', function (req, res) {
+  const author = req.params.author;
+  const matchingBooks = Object.values(books).filter(book => book.author === author);
+  return res.status(200).json(matchingBooks);
+});
+
+public_users.get('/books/title/:title', function (req, res) {
+  const title = req.params.title;
+  const matchingBooks = Object.values(books).filter(book => book.title === title);
+  return res.status(200).json(matchingBooks);
 });
 
 module.exports.general = public_users;
---
+++ login
@@ -0,0 +1,5 @@
+curl.exe -X POST -H "Content-Type: application/json" -d "{\"username\":\"user2\", \"password\":\"password1\"}" -c cookies.txt http://localhost:5000/customer/login
+
+OUTPUT:
+
+User successfully logged in
